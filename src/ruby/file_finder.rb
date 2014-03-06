ec1ef987250d7cc1377dd38eb3487db0b0a88b56
Jussi Nieminen <nieminen.jussi@gmail.com>
2014-03-06 11:21:16 +0100

    directory crawler in ruby to find them mp3:s

---
+++ src/ruby/file_finder.rb
@@ -0,0 +1,36 @@
+
+class FileLister
+  def get_full_dirs(dir, root)
+    new_dirs = dir.select do |x| File.directory?(File.join(root, x)) end
+    new_dirs.reject! do |x| ['.', '..'].include?(x) end
+    new_dirs.map! do |x| File.join(root, x) end
+  end
+
+  def get_full_files(dir, root)
+    new_files = dir.select do |x| File.file?(File.join(root, x)) end
+    new_files.map! do |x| File.join(root, x) end
+  end
+
+  def dir_crawler(dirs, files, ending)
+    matching = files.select do |f| File.extname(f) == ending end
+    if dirs.empty?
+      return matching
+    end
+    root = dirs[0]
+    d = Dir.new(root)
+    dirs += get_full_dirs(d, root)
+    new_files = get_full_files(d, root)
+    matching += dir_crawler(dirs[1..-1], new_files, ending)
+    return matching
+  end
+
+  def get_files(root_path, ending)
+    d = Dir.new(root_path)
+    dirs = get_full_dirs(d, root_path)
+    files = get_full_files(d, root_path)
+    dir_crawler(dirs, files, ending)
+  end
+end
+
+# lister = FileLister.new
+# puts lister.get_files(ARGV[0], '.mp3')
